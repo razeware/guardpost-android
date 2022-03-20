120dda50c4f0b0a9ca751b05263316bee0869312
Harun Wangereka <harunwangereka@gmail.com>
2022-03-20 06:26:34 +0300

    Remove Deprecated Android Extensions

--- app/src/main/java/com/raywenderlich/guardpost/MainActivity.kt
+++ app/src/main/java/com/raywenderlich/guardpost/MainActivity.kt
@@ -4,10 +4,11 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import androidx.localbroadcastmanager.content.LocalBroadcastManager
+import com.raywenderlich.guardpost.databinding.ActivityMainBinding
 import com.raywenderlich.guardpost.utils.randomString
-import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+  private lateinit var activityMainBinding: ActivityMainBinding
 
   private val localBroadcastManager by lazy(LazyThreadSafetyMode.NONE) {
     LocalBroadcastManager.getInstance(this)
@@ -15,23 +16,25 @@ class MainActivity : AppCompatActivity() {
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-    setContentView(R.layout.activity_main)
+    activityMainBinding = ActivityMainBinding.inflate(layoutInflater)
+    val view = activityMainBinding.root
+    setContentView(view)
 
     val clientApiKey = getString(R.string.client_api_key)
     val nonce = randomString()
 
-    login.setOnClickListener {
+    activityMainBinding.login.setOnClickListener {
       GuardpostAuth.startLogin(this, clientApiKey, nonce)
     }
 
-    logout.setOnClickListener {
+    activityMainBinding.logout.setOnClickListener {
       GuardpostAuth.startLogout(this)
     }
 
     val guardpostAuthReceiver = GuardpostAuthReceiver()
 
     guardpostAuthReceiver.login.observe(this, Observer {
-      textView.text = it.toString()
+      activityMainBinding.textView.text = it.toString()
     })
 
     localBroadcastManager.registerReceiver(
